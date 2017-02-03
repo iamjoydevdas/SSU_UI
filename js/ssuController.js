a5a6b293a67c8aeac4269b6eee89240cd28b9f33
iamjoydevdas <iamjoydevdas@gmail.com>
2017-02-03 06:58:14 +0530

    login controller
    Runa

--- js/ssuController.js
+++ js/ssuController.js
@@ -5,11 +5,37 @@ angular.module("ssu").controller('homeController', function(){
 	$scope.firstName = "";
 	$scope.middleName = "";
 	$scope.lastName = "";
-	$scope.displayName = "";
+	$scope.userName = "";
 	$scope.sex = "";
 	$scope.email = "";
 	$scope.pwd = "";
 	$scope.rePwd = "";
 	$scope.seqQ = "";
 	$scope.seqA = "";
-});
+})
+.controller('headerController', function($scope){
+	$rootScope.header = "";
+	$scope.isValidUser = false;
+	$rootScope.$watch('header', function(newVal, oldVal){
+		$rootScope.header = newVal;
+		$scope.isValidUser = true;
+	});
+})
+
+.controller('loginController', function($scope){
+	
+	$scope.usernm = "";
+	$scope.pass="";
+	$scope.login = function(){
+		
+		if ($scope.usernm == "runa" && $scope.pass == "1234"){
+			$rootscope.header= $scope.usernm;
+		}
+		
+	};
+	
+	
+	
+})
+
+;
