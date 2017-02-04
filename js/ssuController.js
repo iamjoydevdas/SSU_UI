a1d81610b83aaefc31d4c55a832aa7773f03706a
iamjoydevdas <iamjoydevdas@gmail.com>
2017-02-04 05:40:36 +0530

    Runa -
    Modal Header color change
    Forgot password Ajax intregration

--- js/ssuController.js
+++ js/ssuController.js
@@ -21,30 +21,122 @@ angular.module("ssu").controller('homeController', ["$rootScope",function($rootS
 		$scope.isValidUser = true;
 	});*/
 	$scope.$on('loginEvent', function (event, data) {
-	    console.log(data.header); // 'Some data'
-	    $scope.header = data.header;
-	    $scope.isValidUser = true;
-	  });
+		console.log(data.header); // 'Some data'
+		$scope.header = data.header;
+		$scope.isValidUser = true;
+	});
 }])
 
 .controller('loginController',["$rootScope","$scope", function($rootScope,$scope){
-	
+
 	$scope.usernm = "";
 	$scope.pass="";
 	$scope.login = function(){
-		
+
 		if ($scope.usernm == "runa" && $scope.pass == "1234"){
 			//$rootScope.header= $scope.usernm;
 			$rootScope.$broadcast('loginEvent', {
-				  header:$scope.usernm // send whatever you want
-				});
+				header:$scope.usernm // send whatever you want
+			});
 		}
-		
-	};
-	
-	
-	
-	
-}])
 
+	};
+
+	$scope.forgotPassword = function(){
+		$rootScope.$broadcast("refreshforgotpass",{
+			refresh : true
+		});
+	};
+
+
+}])
+.controller('forgotPasswordController',['$http', '$scope', function($http, $scope){
+	$scope.seqA="";
+	$scope.$on('refreshforgotpass', function (event, data) {
+		if(data.refresh){
+			$scope.usernm="";
+			$scope.isUserNameBlank = false;
+			$scope.isUserNameIncorrect = false;
+			$scope.isSeqQVisible= false;
+			$scope.resetValue = true;
+			$scope.securityQuestion="";
+			$scope.usernameVisibility = false;
+			$scope.seqANotMatched = false;
+			$scope.seqA="";
+		}
+	});
+
+	$scope.usernm="";
+	$scope.isUserNameBlank = false;
+	$scope.isUserNameIncorrect = false;
+	$scope.isSeqQVisible= false;
+	$scope.resetValue = true;
+	$scope.usernameVisibility = true;
+	$scope.securityQuestion="";
+	$scope.seqANotMatched = false;
+	$scope.checkUserName = function(){
+		if ($scope.usernm == "" ){
+			$scope.isUserNameBlank = true;
+			$scope.isSeqQVisible= false;
+			$scope.isUserNameIncorrect = false;
+			$scope.usernameVisibility = false;
+			$scope.seqANotMatched = false;
+		}
+		else {
+			$http({
+				method : 'POST',
+				url : "http://localhost:8084/ssu_service/rest/ssu/checkUserName"	,
+				params : {
+					'userName' : $scope.usernm
+				}				
+			}).then(function succsess(response){
+				$scope.securityQues = response.data.securityQues;
+				if($scope.securityQues == '-1'){
+					$scope.isUserNameIncorrect = true;
+					$scope.isUserNameBlank = false;
+					$scope.isSeqQVisible = false;
+					$scope.usernameVisibility = false;
+					$scope.seqANotMatched = false;
+				}else{
+					$scope.securityQuestion = $scope.securityQues;
+					$scope.isSeqQVisible = true;
+					$scope.isUserNameIncorrect = false;
+					$scope.isUserNameBlank = false;
+					$scope.usernameVisibility = true;
+					$scope.seqANotMatched = false;
+				}
+			}).then(function fail(response){
+
+			});
+
+		}
+
+	};
+
+	$scope.reset = function(){
+		
+		if($scope.seqA !=""){
+			$http({
+				method : 'POST',
+				url : "http://localhost:8084/ssu_service/rest/ssu/resetPass"	,
+				params : {
+					'userName' : $scope.usernm,
+					'seqA' : $scope.seqA
+				}				
+			}).then(function succsess(response){
+				if(response.data.isAnswerMatched){
+					$scope.resetValue = false
+					
+				}else{
+					$scope.seqA="";
+					$scope.seqANotMatched = true;				}
+			}).then(function fail(response){
+
+			});
+		}
+
+	};
+
+
+}])
 ;
