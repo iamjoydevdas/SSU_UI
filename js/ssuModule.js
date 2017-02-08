b5f9d89680f5d97ca7ba183482f3abca075a7f2d
iamjoydevdas <iamjoydevdas@gmail.com>
2017-02-09 00:15:33 +0530

    loading

--- js/ssuModule.js
+++ js/ssuModule.js
@@ -1,1 +1,6 @@
-angular.module('ssu',['ngRoute','ui.bootstrap','ngAnimate', 'toaster']);
+angular.module('ssu',['ngRoute','ui.bootstrap','ngAnimate', 'toaster'])
+.config(['cfpLoadingBarProvider', function (cfpLoadingBarProvider) {
+    cfpLoadingBarProvider.includeBar = !0;
+    cfpLoadingBarProvider.includeSpinner = !1;
+    cfpLoadingBarProvider.latencyThreshold = 400;
+}]);
